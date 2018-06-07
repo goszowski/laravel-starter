36db6e0d458914f3dadb7e08f274160553c94557
Jaroslaw Goszowski <goszowski@gmail.com>
2018-06-07 17:56:57 +0300

    Laravel-admin installed

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -1,17 +1,24 @@
 <div class="col-md-3">
-    <div class="card">
-        <div class="card-header">
-            Sidebar
-        </div>
+    @foreach($laravelAdminMenus->menus as $section)
+        @if($section->items)
+            <div class="card">
+                <div class="card-header">
+                    {{ $section->section }}
+                </div>
 
-        <div class="card-body">
-            <ul class="nav" role="tablist">
-                <li role="presentation">
-                    <a href="{{ url('/admin') }}">
-                        Dashboard
-                    </a>
-                </li>
-            </ul>
-        </div>
-    </div>
+                <div class="card-body">
+                    <ul class="nav flex-column" role="tablist">
+                        @foreach($section->items as $menu)
+                            <li class="nav-item" role="presentation">
+                                <a class="nav-link" href="{{ url($menu->url) }}">
+                                    {{ $menu->title }}
+                                </a>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+            <br/>
+        @endif
+    @endforeach
 </div>
